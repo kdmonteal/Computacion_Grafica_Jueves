79c5d07f8edd6eaa75cbeb76a8a4a870022abaca
kdmonteal <kdmonteal@gmail.com>
2023-02-23 10:32:36 -0500

    grid creation

--- semana 1 a 4/src/js/main.js
+++ semana 1 a 4/src/js/main.js
@@ -33,6 +33,13 @@ function initScene() {
     renderer = new THREE.WebGLRenderer({canvas: myCanvas});
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
+
+    // Create Grid
+    const size = 10;
+    const divisions = 10;
+
+    const gridHelper = new THREE.GridHelper( size, divisions );
+    scene.add( gridHelper );
 }
 
 function createGeometries() {
