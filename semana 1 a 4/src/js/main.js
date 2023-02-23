16161d05ee184fef178dd4285468fbfd3e41b204
kdmonteal <kdmonteal@gmail.com>
2023-02-23 09:39:47 -0500

    Código Clase

---
+++ semana 1 a 4/src/js/main.js
@@ -0,0 +1,49 @@
+// Principales variables son:
+var scene = null,
+    camera = null,
+    renderer = null,
+    myCanvas = null,
+    controls = null,
+    
+    cube = null;
+
+// Call all functions that allow create 3D
+function start3dService() {
+    initScene();
+    createGeometries();
+    animate();
+}
+
+function initScene() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75,   // FOV (Fild of view)
+                                        window.innerWidth / window.innerHeight, // ASPECT (Size of Screen)
+                                        0.1,  // NEAR (Cerca)
+                                        1000 ); //FAR (lejos)
+
+    myCanvas = document.querySelector('.webgl'); 
+    renderer = new THREE.WebGLRenderer({canvas: myCanvas});
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+}
+
+function createGeometries() {
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+}
+
+function animate() {
+	requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+
+
+console.log(THREE);
